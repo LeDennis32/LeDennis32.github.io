928f3743d51c59e6bafa326b0be1fe926434798b
LeDennis32 <ledennis32@gmail.com>
2023-11-18 01:52:04 +0100

    Added spinning box :3

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function changeColor() {
 	$(".myname").toggleClass("pink");
 }
 
+function setSpinBoxHeight() {
+	spinbox = $(".spinningbox");
+	spinbox.each(function() {
+		$(this).css("height", $(this).css("width"));
+	});
+}
+
 $(function(){
+	// color effect
 	setInterval(changeColor, 700);
+	
+	// spinning box
+	setSpinBoxHeight();
+	$(window).resize(setSpinBoxHeight);
 });
